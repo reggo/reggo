e5fe43753f7d8f3855916d9808e0310ad1ba6361
btracey <bdtinternet@gmail.com>
2014-03-13 12:03:19 -0700

    Added profiling script for neural net

---
+++ nnet/prof/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+
+	"github.com/davecheney/profile"
+
+	"github.com/gonum/floats"
+	"github.com/gonum/matrix/mat64"
+
+	"github.com/reggo/reggo/nnet"
+	"github.com/reggo/reggo/train"
+)
+
+func main() {
+
+	nInputs := 10
+	nOutputs := 3
+	nLayers := 2
+	nNeurons := 50
+	nSamples := 100000
+	nRuns := 50
+
+	defer profile.Start(profile.CPUProfile).Stop()
+
+	net, err := nnet.NewSimpleTrainer(nInputs, nOutputs, nLayers, nNeurons, nnet.Linear{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Generate some random data
+	inputs := mat64.NewDense(nSamples, nInputs, nil)
+	outputs := mat64.NewDense(nSamples, nOutputs, nil)
+	for i := 0; i < nSamples; i++ {
+		for j := 0; j < nInputs; j++ {
+			inputs.Set(i, j, rand.Float64())
+		}
+		for j := 0; j < nOutputs; j++ {
+			outputs.Set(i, j, rand.Float64())
+		}
+	}
+
+	// Create trainer
+	prob := train.NewBatchGradBased(net, true, inputs, outputs, nil, nil, nil)
+	nParameters := net.NumParameters()
+
+	parameters := make([]float64, nParameters)
+	derivative := make([]float64, nParameters)
+
+	for i := 0; i < nRuns; i++ {
+		net.RandomizeParameters()
+		net.Parameters(parameters)
+		prob.ObjGrad(parameters, derivative)
+		fmt.Println(floats.Sum(derivative))
+	}
+}
